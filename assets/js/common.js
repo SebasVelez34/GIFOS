d8dbcdb64263e4cfe3a6c5efed9a802b52991471
Sebastián Vélez <sebastianvelezvelasquez@gmail.com>
2020-08-08 20:43:35 -0500

    Search and autocomplete

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,2 +1,3 @@
-const API_KEY = "api_key=EytZVPh42pEzgkvRtXja72J3N44pAEM5";
-const API_URL = "https://api.giphy.com/v1/gifs";
+String.prototype.toCapitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-
-fetch(`${API_URL}/search?${API_KEY}&q=rabbit&limit=25&offset=0&rating=g&lang=en`)
-.then(function(response) {
-    return response.json();
-})
-.then(function(myJson) {
-    console.log(myJson);
-});
