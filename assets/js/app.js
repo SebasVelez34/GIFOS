625f0c96cc95615efabf35fb3d641a5d27601ca4
Sebastián Vélez <sebastianvelezvelasquez@gmail.com>
2020-08-24 20:59:01 -0500

    Ver favoritos

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,6 @@ const userGIFS = (function () {
     }
 })();
 
-
 const card = (function () {
     function cardFunctions(data){
         const isMobile = userDevice();
@@ -113,75 +112,9 @@ const card = (function () {
     }
 })();
 
-const home = (function () {
+const trending = (function(){
     function __constructor(){
-        categoryTags();
         trendingGIF();
-        clearSearch();
-
-        document.querySelector('#searchGif').onkeyup = (e)=>{
-            autocomplete();
-            let value = searchValue();
-            if (e.key === 'Enter' && value != "") {
-                renderResults(value);
-                searchRender(false);
-            }
-        };
-    }
-
-    function searchValue() {
-        return document.querySelector('#searchGif').value;
-    }
-
-    function clearSearch() {
-        document.querySelector('#img-close').onclick = ()=>{
-            document.querySelector('#searchGif').value = '';
-            searchRender(false);
-        };
-    }
-    function searchRender(show = false) {
-        const parent = document.querySelector('#search');
-        parent.classList.add('active');
-        show ? parent.classList.add('active') : parent.classList.remove('active');
-    }
-    async function categories() {
-        return await fetch(`${API_URL}/categories?${API_KEY}`)
-        .then((response) => response.json())
-        .then(data =>{
-            return data.data;
-        });
-    }
-
-    async function autocomplete(){
-        const parent = document.querySelector('#search');
-        const ul     = document.querySelector('#search-results ul');
-        const data   = await fetch(`${API_URL}/search/tags?${API_KEY}&q=${searchValue()}`)
-                            .then((response) => response.json())
-                            .then(data =>{
-                                return data.data;
-                            });
-        searchRender(true);
-        ul.innerHTML = '';
-        if (data.length > 0) {
-            data.map(option =>{
-                let li         = document.createElement('li');
-                let img        = document.createElement('img');
-                let optionName = document.createTextNode(option.name);
-                img.src        = './assets/images/icon-search.svg';
-                li.onclick     = () => { results(option) };
-                li.appendChild(optionName);
-                li.prepend(img);
-                ul.appendChild(li);
-            });
-        }else{
-            ul.insertAdjacentHTML('afterbegin',`<li><img src="./assets/images/icon-search.svg" />Sin resultados</li>`);
-        }
-    }
-
-    function results({name}) {
-        renderResults(name);
-        document.querySelector('#searchGif').value = name;
-        searchRender(false);
     }
     async function dataTrending(limit = 10) {
         return await fetch(`${API_URL}/trending?${API_KEY}&limit=${limit}`)
@@ -205,59 +138,11 @@ const home = (function () {
             });
         })
     }
-
-    async function categoryTags() {
-        categories().then(data => {
-            const categories = data.slice(0,5);
-            const parent     = document.querySelector(".trending .tags");
-            categories.map(category => {
-                let a       = document.createElement('a');
-                const name  = category.name.toCapitalize();
-                a.onclick   = () => { renderResults(name) };
-                a.innerHTML = `${name},`;
-                parent.appendChild(a);
-            });
-        });
-    }
-
-    async function searchGIFS(term,limit = 12,offset = 0) {
-        return await fetch(`${API_URL}/search?${API_KEY}&q=${term}&limit=${limit}&offset=${offset}`)
-                            .then((response) => response.json())
-                            .then(data =>{
-                                return data.data;
-                            });
-    }
-
-    function renderResults(term,offset = 0){
-        const parent    = document.querySelector('#results');
-        const container = parent.querySelector('.container-results');
-        searchGIFS(term,12,offset).then(data =>{
-            parent.classList.remove('d-none');
-            parent.querySelector('#result-title').innerHTML = term;
-            if (offset == 0) container.innerHTML = '';
-            data.map(gif => {
-                let { url }  = gif.images.preview_webp;
-                let img     = document.createElement('img');
-                img.src     = url;
-                img.onclick = ()=>{
-                    card.options(gif);
-                };
-                container.appendChild(img);
-            });
-            if(data)
-                parent.querySelector('#load-more').onclick = ()=>{ renderResults(term,offset + 12); };
-            else
-                parent.querySelector('#load-more').classList.add('d-none');
-
-        })
-    }
-
     return {
         init : __constructor,
     }
 })();
 
-
-
-
-home.init();
+(function(){
+    trending.init();
+})();
---
+++ assets/js/favorites.js
@@ -0,0 +1,37 @@
+const favorites = (function () {
+    function render(prevOffset = 0,offset = 12,pag = 1) {
+        let userGifs    = userGIFS.getGIFS();
+        pagination(userGifs.length,offset,pag);
+        userGifs        = userGifs.slice(prevOffset,offset);
+        const parent    = document.querySelector('#results');
+        const container = parent.querySelector('.container-results');
+        userGifs.map(gif => {
+            let { url } = gif.images.preview_webp || gif.images.original;
+            let img     = document.createElement('img');
+            img.src     = url;
+            img.onclick = ()=>{
+                card.options(gif);
+            };
+            container.appendChild(img);
+        });
+        if(userGifs)
+            parent.querySelector('#load-more').onclick = ()=>{ render(offset,offset + 12,pag + 1); };
+        else
+            parent.querySelector('#load-more').classList.add('d-none');
+    }
+
+    function pagination(length,offset,pag) {
+        let pagination  = Math.ceil(length / offset);
+        if (pag <= pagination)
+            document.querySelector('#load-more').classList.remove('d-none');
+        else
+            document.querySelector('#load-more').classList.add('d-none');
+    }
+    return{
+        render
+    }
+})();
+
+(function() {
+    favorites.render();
+})();
